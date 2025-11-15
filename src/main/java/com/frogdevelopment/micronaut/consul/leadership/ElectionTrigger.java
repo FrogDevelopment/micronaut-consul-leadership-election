444de52c64c91abc330f9fc30c477716246f2a1b
Le Gall, Benoit <legall.benoit@gamil.com>
2025-11-15 21:56:54 +0100

    use Singleton instead of Prototype

--- src/main/java/com/frogdevelopment/micronaut/consul/leadership/ElectionTrigger.java
+++ src/main/java/com/frogdevelopment/micronaut/consul/leadership/ElectionTrigger.java
@@ -2,9 +2,10 @@ package com.frogdevelopment.micronaut.consul.leadership;
 
 import lombok.extern.slf4j.Slf4j;
 
+import jakarta.inject.Singleton;
+
 import com.frogdevelopment.micronaut.consul.leadership.election.LeaderElectionOrchestrator;
 
-import io.micronaut.context.annotation.Prototype;
 import io.micronaut.context.event.ShutdownEvent;
 import io.micronaut.context.event.StartupEvent;
 import io.micronaut.runtime.event.annotation.EventListener;
@@ -15,7 +16,7 @@ import io.micronaut.runtime.event.annotation.EventListener;
  * @since 1.0.0
  */
 @Slf4j
-@Prototype
+@Singleton
 public final class ElectionTrigger {
 
     /**
