4d45af8c59a898e1cb54054a1487d3bc1ba841c3
Le Gall, Benoit <legall.benoit@gamil.com>
2025-11-16 19:35:11 +0100

    [micronaut] upgrade Gradle plugin

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,7 +1,7 @@
 plugins {
     // Apply the foojay-resolver plugin to allow automatic download of JDKs
     id("org.gradle.toolchains.foojay-resolver-convention") version "1.0.0"
-    id("io.micronaut.platform.catalog") version "4.6.0"
+    id("io.micronaut.platform.catalog") version "4.6.1"
 }
 
 enableFeaturePreview("TYPESAFE_PROJECT_ACCESSORS")
